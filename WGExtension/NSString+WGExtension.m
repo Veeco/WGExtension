3df9e0918edde5e1a7c6ed6ed2c752027850c3aa
Veeco <298059748@qq.com>
2017-04-05 10:38:55 +0800

    Update NSString+WGExtension.m

--- WGExtension/NSString+WGExtension.m
+++ WGExtension/NSString+WGExtension.m
@@ -13,9 +13,12 @@
  * 计算出文字总体高度
  * 参数 width 文字总体宽度
  * 返回 文字总体高度
+ * 注意 传入的宽度与字体大小要大于0且宽度不小于字体大小
  */
 - (CGFloat)calculateStringHeightWithWidth:(CGFloat)width fontSize:(CGFloat)fontSize {
 
+        if (width <= 0.0 || fontSize <= 0.0 || width < fontSize) [NSException raise:@"计算文字高度出错" format:@"请检查传入的宽度与字体大小是否大于0且宽度不小于字体大小"];
+    
     // 设置属性
     NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:fontSize]};
     
