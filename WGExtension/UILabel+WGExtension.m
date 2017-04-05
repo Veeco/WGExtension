0029783ea92e82616f14002447dc70199da03bcb
Veeco <298059748@qq.com>
2017-04-05 11:08:02 +0800

    Update UILabel+WGExtension.m

--- WGExtension/UILabel+WGExtension.m
+++ WGExtension/UILabel+WGExtension.m
@@ -14,7 +14,10 @@
  * 参数 string 要设置的文字
  * 注意 要先设置了label宽度后再调用(本质是利用富文本属性) 暂时不支持英文字符
  */
-- (void)wg_averageKernWithString:(NSString *)string {
+- (void)wg_averageKernWithString:(nonnull NSString *)string {
+    
+    // 过滤
+    if (![string isKindOfClass:[NSString class]] || !string) return;
     
     // 算出间距
     CGFloat margin = (self.bounds.size.width - self.font.pointSize * string.length) / (string.length - 1);
