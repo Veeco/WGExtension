0e735c4c8d021fc1ba6b01ca028876f3dada4f59
Veeco <Jianwei>
2017-11-16 16:11:14 +0800

    优化命名

--- WGExtension/Macro/ColorHeader.h
+++ WGExtension/Macro/ColorHeader.h
@@ -10,11 +10,12 @@
 #define ColorHeader_h
 
 // 颜色设定
-#define ColorFromRGB(rgbValue) [UIColor colorWithRed:(rgbValue & 0xFF0000 >> 16) / 255.0f green:(rgbValue & 0xFF00 >> 8) / 255.0f blue:(rgbValue & 0xFF) / 255.0f alpha:1.0f] // RGB
-#define ColorFromRGBA(rgbValue, alphaValue) [UIColor colorWithRed:(rgbValue & 0xFF0000 >> 16) / 255.0f green:(rgbValue & 0xFF00 >> 8) / 255.0f blue:(rgbValue & 0xFF) / 255.0f alpha:alphaValue / 1.0f] // RGBA
-#define ColorWithRGBA(r, g, b, a) [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:a / 1.0f] // RGBA
+#define COLOR_FROM_RGB(rgbValue) [UIColor colorWithRed:(rgbValue & 0xFF0000 >> 16) / 255.0f green:(rgbValue & 0xFF00 >> 8) / 255.0f blue:(rgbValue & 0xFF) / 255.0f alpha:1.0f] // RGB
+#define COLOR_FROM_RGBA(rgbValue, alphaValue) [UIColor colorWithRed:(rgbValue & 0xFF0000 >> 16) / 255.0f green:(rgbValue & 0xFF00 >> 8) / 255.0f blue:(rgbValue & 0xFF) / 255.0f alpha:alphaValue / 1.0f] // RGBA
+#define COLOR_WITH_RGBA(r, g, b, a) [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:a / 1.0f] // RGBA
 
 // 颜色方案
-#define Color_Main ColorWithRGBA(94, 184, 106, 1) // 主色
+#define COLOR_MAIN COLOR_WITH_RGBA(94, 184, 106, 1) // 主色
+#define COLOR_BG COLOR_WITH_RGBA(230, 230, 230, 1) // 主色
 
 #endif /* ColorHeader_h */
--- WGExtension/Macro/UtilHeader.h
+++ WGExtension/Macro/UtilHeader.h
@@ -11,65 +11,65 @@
 
 // 打印
 #ifdef DEBUG
-#define Log(FORMAT, ...) fprintf(stderr, "%s : %d\t%s\n", [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
-#define LogFunc NSLog(@"%s", __func__);
+#define LOG(FORMAT, ...) fprintf(stderr, "%s : %d\t%s\n", [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#define LOG_FUNC NSLog(@"%s", __func__);
 #else
-#define Log(...)
-#define LogFunc
+#define LOG(...)
+#define LOG_FUNC
 #endif
 
 // 窗口
-#define KeyWindow UIApplication.sharedApplication.keyWindow
-#define ScreenWidth UIScreen.mainScreen.bounds.size.width
-#define ScreenHeight UIScreen.mainScreen.bounds.size.height
+#define KEY_WINDOW UIApplication.sharedApplication.keyWindow
+#define SCREEN_WIDTH UIScreen.mainScreen.bounds.size.width
+#define SCREEN_HEIGHT UIScreen.mainScreen.bounds.size.height
 
 // 屏幕类型
-#define Screen4_0 (ScreenWidth == 320.0f) // 320 * 568 -> 640 * 1136
-#define Screen4_7 (ScreenWidth == 375.0f) // 375 * 667 -> 750 * 1334
-#define Screen5_5 (ScreenWidth == 414.0f) // 414 * 736 -> 1242 * 2208 -> 1080 * 1920
-#define Screen5_8 (ScreenHeight == 812.0f) // 375 * 812 -> 1125 * 2436
+#define SCREEN_4_0 (SCREEN_WIDTH == 320.0f) // 320 * 568 -> 640 * 1136
+#define SCREEN_4_7 (SCREEN_WIDTH == 375.0f) // 375 * 667 -> 750 * 1334
+#define SCREEN_5_5 (SCREEN_WIDTH == 414.0f) // 414 * 736 -> 1242 * 2208 -> 1080 * 1920
+#define SCREEN_5_8 (SCREEN_HEIGHT == 812.0f) // 375 * 812 -> 1125 * 2436
 
 // 系统版本
-#define iOS_since(version) @available(iOS version, *)
+#define IOS_SINCE(VERSION) @available(iOS VERSION, *)
 
 // 常用区域尺寸
-#define StatusBarHeight (Screen5_8 ? 44.0f : 20.0f) // 状态栏高度
-#define NavBarHeight (44.0f + StatusBarHeight) // 导航栏高度(包含状态栏)
-#define BottomSafeMargin (Screen5_8 ? 34.0f : 0.0f) // 底部安全间隙
-#define TabBarHeight (49.0f + BottomSafeMargin) // 标签栏高度(包含底部安全间隙)
+#define STATUS_BAR_HEIGHT (SCREEN_5_8 ? 44.0f : 20.0f) // 状态栏高度
+#define NAV_BAR_HEIGHT (44.0f + STATUS_BAR_HEIGHT) // 导航栏高度(包含状态栏)
+#define BOTTOM_SAFE_MARGIN (SCREEN_5_8 ? 34.0f : 0.0f) // 底部安全间隙
+#define TAB_BAR_HEIGHT (49.0f + BOTTOM_SAFE_MARGIN) // 标签栏高度(包含底部安全间隙)
 
 // 获取对应比例的像素(参数 x 为 4.7屏 下的开发像素)
-#define ScreenScale(value) (value / 375.0f * ScreenWidth)
-#define SS(value) ScreenScale(value)
+#define SCREEN_SCALE(VALUE) (VALUE / 375.0f * SCREEN_WIDTH)
+#define SS(VALUE) SCREEN_SCALE(VALUE)
 
 // 字体
-#define FontSize(size) [UIFont systemFontOfSize:size]
-#define BoldSize(size) [UIFont boldSystemFontOfSize:size]
+#define FONT_SIZE(SIZE) [UIFont systemFontOfSize:SIZE]
+#define BOLD_SIZE(SIZE) [UIFont boldSystemFontOfSize:SIZE]
 
 // 系统
-#define AppBuild NSBundle.mainBundle.infoDictionary[@"CFBundleVersion"] // 构建版本
-#define AppVersion NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"] // 发布版本
+#define APP_BUILD NSBundle.mainBundle.infoDictionary[@"CFBundleVersion"] // 构建版本
+#define APP_VERSION NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"] // 发布版本
 
 // 弱指针
-#define Weak(object) __weak __typeof__(object) weak##_##object = object;
-#define Strong(object) __strong __typeof__(object) object = weak##_##object;
+#define WEAK(OBJECT) __weak __typeof(OBJECT) WEAK##_##OBJECT = OBJECT;
+#define STRONG(OBJECT) __strong __typeof(OBJECT) OBJECT = WEAK##_##OBJECT;
 
 // 单例
-#define Singleton(Class) \
-static __kindof Class * _instance; \
+#define SINGLETON(CLASS) \
+static __kindof CLASS * kInstance; \
 \
-+ (__kindof Class *)shared { \
++ (__kindof CLASS *)shared { \
 \
 return [[self alloc] init]; \
 } \
 \
-+ (__kindof Class *)allocWithZone:(struct _NSZone *)zone { \
++ (__kindof CLASS *)allocWithZone:(struct _NSZone *)zone { \
 \
 static dispatch_once_t onceToken; \
 dispatch_once(&onceToken, ^{ \
-_instance = [super allocWithZone:zone]; \
+kInstance = [super allocWithZone:zone]; \
 }); \
-return _instance; \
+return kInstance; \
 }
 
 #endif /* UtilHeader_h */
